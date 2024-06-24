3228155ca234e18d9ce96c776b3c99f39ab1532c
Josenei Santana <joseneisatana@yahoo.com>
2024-06-24 13:25:01 -0300

    Finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("Ola joseney")
+AOS.init();
+
+const dataDovento= new Date("dec12, 2023  19:00");
+const timeStaempDoevento = dataDovento.getTime ();
+
+const contaAsHoras = setInterval( function()  {
+    const agora= new Date();
+    const timeStaempAtual = agora.getTime();
+
+        const distanciaAteOEvento = timeStaempDoevento - timeStaempAtual;
+
+        const diaEmMS = 1000 * 60 * 60 * 24;
+        const horaEmMS = 1000 * 60 * 60 ;
+        const minutoEmMS = 1000 * 60 ;
+
+
+        const diasAteOEvento = Math.floor( distanciaAteOEvento / diaEmMS  );
+        const horasAteOEvento =Math.floor((distanciaAteOEvento %horaEmMS) / horaEmMS);
+        const minutosAteOEvento =Math.floor((distanciaAteOEvento % minutoEmMS) /horaEmMS   )  ;
+        const segundosAteOEvento =Math.floor((distanciaAteOEvento%  minutoEmMS ) / 1000 );
+
+            document.getElementById('contador').innerHTML =`${diasAteOEvento}d  ${horasAteOEvento}h ${minutosAteOEvento}m  ${segundosAteOEvento}s `
+
+
+            if(distanciaAteOEvento < 0){
+                clearInterval(contaAsHoras);
+                document.getElementById('contador').innerHTML ='Evento expirado'
+            }
+
+}, 1000);
